0411de6a1982b76dc91ab0ffff8eaf051d5872c0
Larry Li <llfireboy@gmail.com>
2016-12-11 11:49:30 -0600

    Updated if statement
    Changed " to ' and = to ==

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,7 @@
 import Calculator.CalculatorProperties;
 class Calculator{
 public static int main( int num1, char sign, int num2) {
-if (sign = "+") {
+if (sign == '+') {
   add(num1, num2);
 };
 };
