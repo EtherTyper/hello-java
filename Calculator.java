d8a40ea7241f7db1116f99007ce9e1af34011fb2
Larry Li <llfireboy@gmail.com>
2016-12-11 20:37:19 -0600

    Add the add

--- Calculator.java
+++ Calculator.java
@@ -7,6 +7,6 @@ Scanner input = new Scanner(System.in);
 CustomObject add = new CustomObject();
 System.out.println("Input:");
 String i = input.nextLine;
-System.out.println(i);
+System.out.println(add(input,1));
 };
 }
