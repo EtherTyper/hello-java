ae7a6ba203c8ad6ab1cc5ad4c649917d7dd97972
Larry Li <llfireboy@gmail.com>
2016-12-11 21:28:24 -0600

    import try

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
-
+import Calculator.CalcOperations;
 class Calculator{
 public static void main(String[] args) {
 Scanner input = new Scanner(System.in);
-CustomObject add = new CustomObject();
 System.out.println("Input:");
 String i = input.nextLine;
 System.out.println(add(input,1));
